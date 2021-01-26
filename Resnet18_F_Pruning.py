83477a8cbe99c22a939712362b68caec8e0ba3bb
Allen Ding <allending@Allens-MacBook-Pro.local>
2021-01-25 21:53:10 -0600

    idk

--- Resnet18_F_Pruning.py
+++ Resnet18_F_Pruning.py
@@ -2,119 +2,6 @@ import torch
 import torchvision
 import torchvision.datasets as datasets
 from torch import nn, optim
-import torch.nn.functional as F
-
-class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, in_planes, planes, stride=1, L=1):
-        super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        
-        # Normalising factor derived in Stable Resnet paper
-        # https://arxiv.org/pdf/2002.08797.pdf
-        self.factor = L**(-0.5)
-        
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion*planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
-            )
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
-        out = out*self.factor + self.shortcut(x)
-        out = F.relu(out)
-        return out
-
-
-class Bottleneck(nn.Module):
-    expansion = 4
-
-    def __init__(self, in_planes, planes, stride=1, L=1):
-        super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
-
-        # Normalising factor derived in Stable Resnet paper
-        # https://arxiv.org/pdf/2002.08797.pdf
-        self.factor = L**(-0.5)
-        
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion*planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
-            )
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = F.relu(self.bn2(self.conv2(out)))
-        out = self.bn3(self.conv3(out))
-        out = out*self.factor + self.shortcut(x)
-        out = F.relu(out)
-        return out
-
-
-class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10, temp=1.0, in_planes=64, stable_resnet=False):
-        super(ResNet, self).__init__()
-        self.in_planes = in_planes
-        if stable_resnet:
-            # Total number of blocks for Stable ResNet
-            # https://arxiv.org/pdf/2002.08797.pdf
-            L = 0
-            for x in num_blocks:
-                L+=x
-            self.L = L
-        else:
-            self.L = 1
-        
-        self.masks = None
-
-        self.conv1 = nn.Conv2d(3, self.in_planes, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(self.in_planes)
-        self.layer1 = self._make_layer(block, in_planes, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, in_planes*2, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, in_planes*4, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, in_planes*8, num_blocks[3], stride=2)
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(in_planes*8*block.expansion, num_classes)
-        self.temp = temp
-
-    def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1]*(num_blocks-1)
-        layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride, self.L))
-            self.in_planes = planes * block.expansion
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = self.avgpool(out)
-        out = torch.flatten(out, 1)
-        out = self.fc(out) / self.temp
-        
-        return out
-            
-
-def resnet18(temp=1.0, **kwargs):
-    model = ResNet(BasicBlock, [2, 2, 2, 2], temp=temp, **kwargs)
-    return model
 
 I_model = torch.load("../Allen_UROP/data/introspection.txt")
 
@@ -128,9 +15,9 @@ cifar10_test = datasets.CIFAR10(root = "../Allen_UROP/datasets", train=False, do
 trainloader = torch.utils.data.DataLoader(cifar10_train, batch_size=batch, shuffle= True )
 testloader = torch.utils.data.DataLoader(cifar10_test, batch_size = 10000, shuffle = True)
 
-simple_model = resnet18()
+simple_model = torchvision.models.resnet18()
 simple_model.to(cuda)
-testoptimizer = optim.SGD(simple_model.parameters(), .001, weight_decay=.0001)
+testoptimizer = optim.SGD(simple_model.parameters(), .1, weight_decay=.0001)
 testcriterion = nn.CrossEntropyLoss()
 
 def dict_to_vec(model_skel):
@@ -196,6 +83,7 @@ def prune(weights, iteration, p, zeros, mask):
       conv_layers[item][conv_layers[item] > threshold] = 1
       conv_layers[item][conv_layers[item] < -threshold] = 1
       conv_layers[item][conv_layers[item] != 1] = 0
+      print(conv_layers[item])
   zeros += pruned
   print("pruning finished", pruned)
   return (conv_layers,zeros)
