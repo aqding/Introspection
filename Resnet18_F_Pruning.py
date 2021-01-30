4f521f19e022d07d72273030d85f95a947bd7699
Allen Ding <allending@Allens-MacBook-Pro.local>
2021-01-29 19:37:34 -0600

    removed images and ready for testing

--- Resnet18_F_Pruning.py
+++ Resnet18_F_Pruning.py
@@ -29,7 +29,7 @@ train_transform = transforms.Compose([
     normalize,
 ])
 
-epochs = 80
+epochs = 160
 batch = 128
 # Steps per epoch (CIFAR): 391
 cifar10_train = datasets.CIFAR10(root = "../Allen_UROP/datasets", train=True, download = True, transform = train_transform)
@@ -39,7 +39,7 @@ testloader = torch.utils.data.DataLoader(cifar10_test, batch_size = 100, shuffle
 
 simple_model = ResNet18()
 simple_model.to(cuda)
-testoptimizer = optim.SGD(simple_model.parameters(), .1, weight_decay=.0001)
+testoptimizer = optim.SGD(simple_model.parameters(), .1, momentum =.9, weight_decay=.0001)
 testcriterion = nn.CrossEntropyLoss()
 
 def dict_to_vec(model_skel):
@@ -82,9 +82,8 @@ def prune(weights, iteration, p, total_pruned, mask):
   for item in conv_layers:
       conv_layers[item][torch.abs(conv_layers[item]) > threshold] = 1
       conv_layers[item][conv_layers[item] != 1] = 0
-  
   total_pruned += pruned
-  print("pruning finished", pruned, "weights pruned for a total of", total_pruned, "weights")
+  print("Pruning finished", pruned, "weights pruned, for a total of", total_pruned, "weights")
   return (conv_layers,total_pruned)
 
 prunable_layers = [name + '.weight' for name, module in simple_model.named_modules() if
@@ -101,7 +100,7 @@ total_model_size = torch.reshape(dict_to_vec(simple_model.state_dict()), (-1, ))
 print(total_model_size, total_size)
 
 # Precomputed values, corresponds to the number of steps for 40/80 epochs, respectively
-important_steps = [1*391, 2*391]
+important_steps = [160*391, 320*391]
 timesteps = []
 for step in important_steps:
   timesteps.append(step)
@@ -114,7 +113,7 @@ for i in range(len(timesteps)):
   if(i+1 % 4 == 0):
     new_dict = {}
     for item in mask:
-      new_dict[item] = init_state_dict[item]
+      new_dict[item] = init_state_dict[item].clone().detach()
     weight_holder[i] = dict_to_vec(new_dict)
 
 num_zeroed = 0
@@ -125,6 +124,7 @@ mask_as_list = []
 inserts = 0
 for i in range (3):
   print("Pruning Step", i+1)
+  max_accuracy = 0
   for m in range(epochs):
     for batch_num, (data, labels) in enumerate(trainloader):
       testoptimizer.zero_grad()
@@ -137,25 +137,15 @@ for i in range (3):
       output = simple_model(data)
       loss = testcriterion(output, labels)
       loss.backward()
-      # mask_list_counter = 0
-      # for parameter in simple_model.parameters():
-      #   if(mask_list_counter < len(mask_as_list)):
-      #     if(parameter.size() == mask_as_list[mask_list_counter].size()):
-      #         parameter.grad = parameter.grad*mask_as_list[mask_list_counter]
-      #         mask_list_counter += 1
       testoptimizer.step()
-      # state_dict = simple_model.state_dict()
-      # for item in mask:
-      #   state_dict[item] = state_dict[item].to(cuda)*mask[item]
-      # simple_model.load_state_dict(state_dict)
       counter += 1
-      # for k in range(len(timesteps)):
-      #   if(timesteps[k] == counter):
-      #     x = simple_model.state_dict()
-      #     new_dict = {}
-      #     for item in mask:
-      #       new_dict[item] = x[item]
-      #     weight_holder[k] = dict_to_vec(new_dict)
+      for k in range(len(timesteps)):
+        if(timesteps[k] == counter):
+          x = simple_model.state_dict()
+          new_dict = {}
+          for item in mask:
+            new_dict[item] = x[item].clone().detach()
+          weight_holder[k] = dict_to_vec(new_dict)
     correct = 0
     for name, param in simple_model.named_parameters():
         if(name in mask):
@@ -167,18 +157,20 @@ for i in range (3):
       correct += validate(test_output, test_labels)
     training_iterations.append(counter)
     accuracy.append(correct/10000)
+    if(correct/10000 > max_accuracy):
+      max_accuracy = correct/10000
     print("Epoch", m, "acc", correct/10000)
     for k in range(len(important_steps)):
       if(important_steps[k] == counter):
           print("Pruning...", k+1)
-          #acceleration = I_model(torch.transpose(weight_holder[4*k:4*k+4, :], 0, 1)*1000)/1000
-          accelerate_dict = {}
-          for item in mask:
-              accelerate_dict[item] = simple_model.state_dict()[item]
-          #accelerate_dict = vec_to_dict(acceleration, mask)
-          for item in accelerate_dict:
-            accelerate_dict[item] = accelerate_dict[item].to(cuda)*mask[item]
-          (mask, num_zeroed) = prune(accelerate_dict, k+1, .5, num_zeroed, mask)
+          acceleration = I_model(torch.transpose(weight_holder[4*k:4*k+4, :], 0, 1)*1000)/1000
+          # accelerate_dict = {}
+          # for item in mask:
+          #     accelerate_dict[item] = simple_model.state_dict()[item]
+          accelerate_dict = vec_to_dict(acceleration, mask)
+          # for item in accelerate_dict:
+          #   accelerate_dict[item] = accelerate_dict[item].to(cuda)*mask[item]
+          (mask, num_zeroed) = prune(accelerate_dict, k+1, .6971, num_zeroed, mask)
     temp_dict = {item: weight.clone().cpu().detach() for item, weight in simple_model.state_dict().items() if item in prunable_layers} 
     zeroed = 0
     mask_zeroed = 0
@@ -187,15 +179,7 @@ for i in range (3):
       zeroed += int(torch.sum(temp_dict[item]))
       mask_zeroed += int(torch.sum(mask[item]))
     print(total_size-zeroed, total_size-mask_zeroed)
-
-          # temp_state_dict = simple_model.state_dict()
-          # mask_as_list = []
-          # for item in mask:
-          #     temp_state_dict[item] = mask[item] * temp_state_dict[item].to(cuda)
-          #     temp_state_dict[item] = temp_state_dict[item].to(cuda)
-          #     mask_as_list.append(mask[item])
-          # simple_model.load_state_dict(temp_state_dict)
-
+  print("Max Accuracy:",max_accuracy)
 
         
 torch.save(accuracy, "../Allen_UROP/data/resnet18_f_prune_accuracy.txt")
--- Resnet18_NF_Pruning.py
+++ Resnet18_NF_Pruning.py
@@ -4,6 +4,7 @@ import torchvision.datasets as datasets
 from torch import nn, optim
 import torch.nn.functional as F
 import torchvision.transforms as transforms
+from Resnet import ResNet18
 
 normalize = transforms.Normalize(
     mean=[0.4914, 0.4822, 0.4465],
@@ -27,9 +28,9 @@ train_transform = transforms.Compose([
 
 I_model = torch.load("../Allen_UROP/data/introspection.txt")
 
-cuda = torch.device("cuda:5")
+cuda = torch.device("cuda:6")
 
-epochs = 80
+epochs = 160
 batch = 128
 # Steps per epoch (CIFAR): 391
 cifar10_train = datasets.CIFAR10(root = "../Allen_UROP/datasets", train = True, download = True, transform = train_transform)
@@ -37,9 +38,9 @@ cifar10_test = datasets.CIFAR10(root = "../Allen_UROP/datasets", train=False, do
 trainloader = torch.utils.data.DataLoader(cifar10_train, batch_size=batch, shuffle= True )
 testloader = torch.utils.data.DataLoader(cifar10_test, batch_size = 10000, shuffle = True)
 
-simple_model = torchvision.models.resnet18()
+simple_model = ResNet18()
 simple_model.to(cuda)
-testoptimizer = optim.SGD(simple_model.parameters(), .1, weight_decay=.0001)
+testoptimizer = optim.SGD(simple_model.parameters(),.1, momentum = .9,weight_decay=.0001)
 testcriterion = nn.CrossEntropyLoss()
 
 def dict_to_vec(model_skel):
@@ -91,39 +92,41 @@ def mergeSort(arr):
             j += 1
             k += 1
 
-def prune(weights, iteration, p, zeros, mask):
-  pruned = int((total_size-zeros)*(p**iteration))
+def prune(weights, iteration, p, total_pruned, mask):
+  # pruned = int((total_size-zeros)*(p**(1/iteration)))
   flat_mask = dict_to_vec(mask)
+  prunable_weights = int(torch.sum(flat_mask))
+  pruned = int(prunable_weights*(p**(1/iteration)))
+  listed_weights = torch.cat([item[mask[name] == 1] for name, item in weights.items()])
+  threshold = torch.sort(torch.abs(listed_weights))[0][pruned]
   conv_layers = {}
   for item in mask:
     conv_layers[item] = weights[item]
-    conv_layers[item] = conv_layers[item].to(cuda)
-  original = torch.abs(dict_to_vec(conv_layers)).tolist()
-  mergeSort(original)
-  threshold = original[zeros+pruned]
+    conv_layers[item] = conv_layers[item].to(cuda) 
+  # original = torch.abs(dict_to_vec(conv_layers)).tolist()
+  # mergeSort(original)
+  # threshold = original[zeros+pruned]
   for item in conv_layers:
-      conv_layers[item][conv_layers[item] > threshold] = 1
-      conv_layers[item][conv_layers[item] < -threshold] = 1
+      conv_layers[item][torch.abs(conv_layers[item]) > threshold] = 1
       conv_layers[item][conv_layers[item] != 1] = 0
-  zeros += pruned
-  print("pruning finished", pruned)
-  return (conv_layers,zeros)
+  total_pruned += pruned
+  print("Pruning finished", pruned, "weights pruned, for a total of", total_pruned, "weights")
+  return (conv_layers,total_pruned)
 
+prunable_layers = [name + '.weight' for name, module in simple_model.named_modules() if
+                isinstance(module, torch.nn.modules.conv.Conv2d) or
+                isinstance(module, torch.nn.modules.linear.Linear)]
 mask = {}
 init_state_dict = simple_model.state_dict()
 for item in init_state_dict:
-  if(item == "conv1.weight" or item == "layer1.0.conv1.weight"
-    or item =="layer1.0.conv2.weight" or item =="layer1.1.conv1.weight" or item=="layer1.1.conv2.weight"
-    or item =="layer2.0.conv1.weight" or item =="layer2.0.conv2.weight" or item=="layer2.1.conv1.weight" or item=="layer2.1.conv2.weight"
-    or item =="layer3.0.conv1.weight" or item =="layer3.0.conv2.weight" or item=="layer3.1.conv1.weight" or item=="layer3.1.conv2.weight"
-    or item =="layer4.0.conv1.weight" or item =="layer4.0.conv2.weight" or item=="layer4.1.conv1.weight" or item=="layer4.1.conv2.weight"):
+  if(item in prunable_layers):
     mask[item] = torch.ones(init_state_dict[item].size()).to(cuda)
 
 total_size = torch.reshape(dict_to_vec(mask), (-1, )).size()[0]
 total_model_size = torch.reshape(dict_to_vec(simple_model.state_dict()), (-1, )).size()[0]
 print(total_model_size, total_size)
 # Precomputed values, corresponds to the number of steps for 40/80 epochs, respectively
-important_steps = [80*391, 160*391]
+important_steps = [160*391, 320*391]
 
 num_zeroed = 0
 counter = 0
@@ -133,55 +136,55 @@ mask_as_list = []
 inserts = 0
 for i in range (3):
   print("Pruning Step", i+1)
+  max_accuracy = 0
   for m in range(epochs):
-    trainloader = torch.utils.data.DataLoader(cifar10_train, batch_size = batch, shuffle = True)
-    iter_train = iter(trainloader)
-    for j in range(50000//batch + 1):
+    for batch_num, (data, labels) in enumerate(trainloader):
       testoptimizer.zero_grad()
-      data, labels = iter_train.next()
       data = data.to(cuda)
       labels = labels.to(cuda)
+      for name, param in simple_model.named_parameters():
+        if(name in mask):
+          param.data *= mask[name]
       output = simple_model(data)
       loss = testcriterion(output, labels)
       loss.backward()
-      mask_list_counter = 0
-      for parameter in simple_model.parameters():
-        if(mask_list_counter < len(mask_as_list)):
-          if(parameter.size() == mask_as_list[mask_list_counter].size()):
-              parameter.grad = parameter.grad*mask_as_list[mask_list_counter]
-              mask_list_counter += 1
       testoptimizer.step()
-      #state_dict = simple_model.state_dict()
-      #for item in mask:
-      #  state_dict[item] = state_dict[item].to(cuda)*mask[item]
-      #simple_model.load_state_dict(state_dict)
       counter += 1
-      #for k in range(len(important_steps)):
-        #if(important_steps[k] == counter):
-            #print("Pruning...", k+1)
-            #current = simple_model.state_dict()
-            #current_convs = {}
-            #for item in mask:
-                #current_convs[item] = current[item]
-            #(mask, num_zeroed) = prune(current_convs, k+1, .8349, num_zeroed, mask)
-            #temp_state_dict = simple_model.state_dict()
-            #mask_as_list = []
-            #for item in mask:
-                #temp_state_dict[item] = mask[item] * temp_state_dict[item].to(cuda)
-                #temp_state_dict[item] = temp_state_dict[item].to(cuda)
-                #mask_as_list.append(mask[item])
-            #simple_model.load_state_dict(temp_state_dict)
-    testloader = torch.utils.data.DataLoader(cifar10_test, batch_size = 10000, shuffle = True)
-    iter_test = iter(testloader)
-    test_data, test_labels = iter_test.next()
-    test_data = test_data.to(cuda)
-    test_labels = test_labels.to(cuda)
-    test_output = simple_model(test_data)
-    correct = validate(test_output, test_labels)
+      for k in range(len(timesteps)):
+        if(timesteps[k] == counter):
+          x = simple_model.state_dict()
+          new_dict = {}
+          for item in mask:
+            new_dict[item] = x[item].clone().detach()
+          weight_holder[k] = dict_to_vec(new_dict)
+    correct = 0
+    for name, param in simple_model.named_parameters():
+        if(name in mask):
+            param.data *= mask[name]
+    for batch_num, (test_data, test_labels) in enumerate(testloader):
+      test_data = test_data.to(cuda)
+      test_labels = test_labels.to(cuda)
+      test_output = simple_model(test_data)
+      correct += validate(test_output, test_labels)
     training_iterations.append(counter)
     accuracy.append(correct/10000)
+    if(correct/10000 > max_accuracy):
+      max_accuracy = correct/10000
     print("Epoch", m, "acc", correct/10000)
-
+    for k in range(len(important_steps)):
+      if(important_steps[k] == counter):
+          print("Pruning...", k+1)
+          weight_dict = {item: weight.clone().cpu().detach() for item, weight in simple_model.state_dict().items() if item in prunable_layers} 
+          (mask, num_zeroed) = prune(weight_dict, k+1, .6971, num_zeroed, mask)
+    temp_dict = {item: weight.clone().cpu().detach() for item, weight in simple_model.state_dict().items() if item in prunable_layers} 
+    zeroed = 0
+    mask_zeroed = 0
+    for item in mask:
+      temp_dict[item][temp_dict[item] != 0] = 1
+      zeroed += int(torch.sum(temp_dict[item]))
+      mask_zeroed += int(torch.sum(mask[item]))
+    print(total_size-zeroed, total_size-mask_zeroed)
+  print("Max Accuracy:",max_accuracy)
 
 torch.save(accuracy, "../Allen_UROP/data/resnet18_nf_prune_accuracy.txt")
 torch.save(training_iterations, "../Allen_UROP/data/resnet18_training_iteration.txt")
